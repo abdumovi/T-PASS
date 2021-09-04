ad0e7f7d99637a4e00aa65f7fb377f3386de655f
abdumovi <abdumovi@gmail.com>
2021-09-04 13:38:26 +0500

    tpass-add

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.work-slider-content').slick({
+    arrows:false,
+    dots:true
+  });
+});
+$(document).ready(function(){
+  $('.grup-slider').slick({
+    arrows:false,
+    dots:true
+  });
+});
